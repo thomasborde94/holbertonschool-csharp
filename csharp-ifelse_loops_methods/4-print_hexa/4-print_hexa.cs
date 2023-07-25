74fd8c721ad08ded396775e901600344fb947d24
thomasborde94 <thomas.borde94@hotmail.fr>
2023-07-25 13:51:52 +0200

    task 4

---
+++ csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 0; number <= 98; number++)
+            {
+                Console.WriteLine(number + " = " + "0x" + number.ToString("x"));
+            }
+        }
+    }
+}
--- csharp-ifelse_loops_methods/README.md
+++ csharp-ifelse_loops_methods/README.md
@@ -4,3 +4,6 @@ Standard if, else if, else statements.
 
 ### Task 2, 3
 Stardard for loop.
+
+### Task 4
+To convert an int into its hexadecimal representation use `number.ToString("x")`.
