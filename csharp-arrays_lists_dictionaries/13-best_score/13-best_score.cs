33606379115f7d266c74f380383d6ab3163c685f
thomasborde94 <thomas.borde94@hotmail.fr>
2023-07-31 13:41:25 +0200

    task 13

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int maxScore = -1;
+        string bestScore = "None";
+        foreach (KeyValuePair<string, int> kvp in myList)
+        {
+            if (kvp.Value > maxScore)
+            {
+                maxScore = kvp.Value;
+                bestScore = kvp.Key;
+            }
+        }
+        return bestScore;
+    }
+}
