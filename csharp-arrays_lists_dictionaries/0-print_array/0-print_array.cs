6b0a64c046ad634503c2cda0cb14c73932b4ea91
thomasborde94 <thomas.borde94@hotmail.fr>
2023-07-31 09:25:46 +0200

    task 1

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int[] newArray = new int[size];
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return newArray;
+        }
+        for (int i = 0; i < size; i++)
+        {
+            newArray[i] = i;
+            if (i != size - 1)
+                Console.Write(i + " ");
+            else
+                Console.WriteLine(i);
+        }
+        return newArray;
+    }
+}
+
