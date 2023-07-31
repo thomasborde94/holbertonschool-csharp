a94bf9025323ed5082f1076e1c87ea9d3b46da79
thomasborde94 <thomas.borde94@hotmail.fr>
2023-07-31 16:51:02 +0200

    task 0

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        List<int> newList = new List<int>();
+        int sum = 0;
+        foreach (int number in myList)
+        {
+            if (newList.Contains(number) == false)
+                newList.Add(number);
+        }
+        for (int i = 0; i < newList.Count; i++)
+            sum += newList[i];
+        return sum;
+    }
+}
---
+++ csharp-hashset_stack_queue_linkedlist/README.md
@@ -0,0 +1,4 @@
+# C# - Data Structures: HashSet, Stack, Queue, LinkedList
+
+### Task 0
+To determine whether an elements is in a list, use `list.Contains()`.
