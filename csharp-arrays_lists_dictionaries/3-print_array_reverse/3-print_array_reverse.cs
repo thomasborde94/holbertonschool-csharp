6a08b2fbe19f47ad2f63e69300b09a45a7947c70
thomasborde94 <thomas.borde94@hotmail.fr>
2023-07-31 09:49:03 +0200

    task 3

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -7,12 +7,14 @@ class Array
         if (array != null)
         {
             for (int i = array.Length - 1; i >= 0; i--)
-        {
-            if (i != 0)
-                Console.Write(array[i] + " ");
-            else
-                Console.WriteLine(array[i]);
+            {
+                if (i != 0)
+                    Console.Write(array[i] + " ");
+                else
+                    Console.WriteLine(array[i]);
+            }
         }
-        }
+        else
+            Console.WriteLine();
     }
 }
