e763c4c26887abca6a8d4b80473dec2b34d83168
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-01 11:05:47 +0200

    task 13

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> current = myLList.First;
+        for (int i = 0; current != null; i++)
+        {
+            if (i == index)
+                myLList.Remove(current);
+            current = current.Next;
+        }
+    }
+}
