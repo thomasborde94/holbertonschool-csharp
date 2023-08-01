34c3a0b41ee48416c4d98e74d53287bcf816edd0
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-01 08:37:42 +0200

    task 3

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Console.WriteLine("Number of items: " + aStack.Count);
+        if (aStack.Count == 0)
+            Console.Write("Stack is empty");
+        else
+            Console.WriteLine("Top item: " + aStack.Peek());
+        Console.WriteLine("Stack contains " + search + ": " + aStack.Contains(search));
+        if (aStack.Contains(search))
+        {
+            while (aStack.Count > 0)
+            {
+                string currentItem = aStack.Peek();
+                aStack.Pop();
+                if (currentItem == search)
+                    break;
+            }
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
--- csharp-hashset_stack_queue_linkedlist/README.md
+++ csharp-hashset_stack_queue_linkedlist/README.md
@@ -5,3 +5,10 @@ To determine whether an elements is in a list, use `list.Contains()`.
 
 ### Task 1
 To sort a list, use `list.Sort()`.
+
+### Task 3
+To create a Stack, use `Stack<string> aStack = new Stack<string>();`.<br>
+To count the items of a stack, use `aStack.Count`.<br>
+To get the top item of a stack, use `var topItem = aStack.Peek()`.<br>
+To remove the top item of a stack, use `aStack.Pop()`.<br>
+To add a new item at the top of a stack, use `aStack.Push()`.
