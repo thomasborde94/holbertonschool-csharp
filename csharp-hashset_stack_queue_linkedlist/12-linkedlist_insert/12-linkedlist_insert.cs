0471dd9b0994b15dbd52d5eac79efa9b018ee9e2
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-01 10:50:52 +0200

    task 12

--- csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -9,10 +9,30 @@ class LList
         LinkedListNode<int> newNode = new LinkedListNode<int>(n);
         while (current != null)
         {
+            // If n is the smallest value
+            if (current.Value == myLList.First.Value)
+                if (n < current.Value)
+                {
+                    myLList.AddBefore(current, newNode);
+                    Console.WriteLine("first if");
+                    break;
+                }
+
             if (n > current.Value && n < current.Next.Value)
-                myLList.AddAfter(current, n);
-            else
-                current = current.Next;
+            {
+                myLList.AddAfter(current, newNode);
+                Console.WriteLine("second if");
+                break;
+            }
+
+            // If n is the biggest value
+            if (n > current.Value && current.Next == null)
+            {
+                myLList.AddAfter(current, newNode);
+                Console.WriteLine("third if");
+                break;
+            }
+            current = current.Next;
         }
         return newNode;
     }
