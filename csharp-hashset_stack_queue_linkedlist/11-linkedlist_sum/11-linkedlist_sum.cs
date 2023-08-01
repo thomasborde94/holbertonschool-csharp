d0683491cbfa2ad1be9da12e033cae1eddd05bb3
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-01 10:04:44 +0200

    task 11

---
+++ csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        LinkedListNode<int> current = myLList.First;
+        int sum = 0;
+        while (current != null)
+        {
+            sum += current.Value;
+            current = current.Next;
+        }
+        return sum;
+    }
+}
