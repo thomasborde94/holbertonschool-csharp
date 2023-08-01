2f13ef6d7525cf8986d24ea3bb93e55f0d4790cf
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-01 09:59:34 +0200

    task 10

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> current = myLList.First;
+        for (int index = 0; current != null; index++)
+        {
+            if (index == n)
+            {
+                return current.Value;
+            }
+            current = current.Next;
+        }
+        return 0;
+    }
+}
