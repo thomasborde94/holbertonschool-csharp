8c203c7d2746595f8523a933d7e7a0b724e38526
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-14 08:11:02 +0200

    task 0

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int printed = 0;
+        for (int i = 0; i < n; i++)
+        {
+            try
+            {
+                Console.WriteLine(myList[i]);
+                printed++;
+            }
+            catch (Exception)
+            {
+            }
+        }
+        return printed;
+    }
+}
