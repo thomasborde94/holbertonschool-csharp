aae6985c2f0cea3c8787c75b7054c8d1496bca93
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-22 16:04:05 +0200

    task 5

---
+++ csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    [TestFixture]
+    public class StrTests
+    {
+        [Test]
+        public void CamelCase_BasicString_ReturnWordCount()
+        {
+            // Arrange
+            string s = "helloMyNameIsJohn";
+            // Act
+            int wordcount = Str.CamelCase(s);
+            // Assert
+            Assert.AreEqual(wordcount, 5);
+        }
+
+        [Test]
+        public void CamelCase_EmptyString_ReturnZero()
+        {
+            // Arrange
+            string s = "";
+            // Act
+            int wordcount = Str.CamelCase(s);
+            // Assert
+            Assert.AreEqual(wordcount, 0);
+        }
+
+        [Test]
+        public void CamelCase_OneWord_ReturnsOne()
+        {
+            // Arrange
+            string s = "hello";
+            // Act
+            int wordcount = Str.CamelCase(s);
+            // Assert
+            Assert.AreEqual(wordcount, 1);
+        }
+    }
+}
---
+++ csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>
+    /// Defines Str Class
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// determines how many words are in a string. Each word begins
+        /// with a capital letter except the first word
+        /// </summary>
+        /// <param name="s">String to analyse</param>
+        /// <returns>Number of words in a string</returns>
+        public static int CamelCase(string s)
+        {
+            if (s.Length == 0 || s == null)
+                return 0;
+            
+            int wordCount = 1;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsUpper(s[i]))
+                    wordCount++;
+            }
+            return wordCount;
+        }
+    }
+}
