af20f4f69ac085806d1709cc8e1d2b7f1814facb
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-22 14:54:38 +0200

    task 3

---
+++ csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void IsPalindrome_SIsPalindrome_ReturnTrue()
+        {
+            // Arrange
+            string s = "A man, a plan, a canal: Panama.";
+            // Act
+            bool isPalindrome = Str.IsPalindrome(s);
+            // Assert
+            Assert.True(isPalindrome);
+        }
+
+        [Test]
+        public void IsPalindrome_SIsNotPalindrome_ReturnFalse()
+        {
+            // Arrange
+            string s = "A man, a plan, a canal: Panama.akx";
+            // Act
+            bool isPalindrome = Str.IsPalindrome(s);
+            // Assert
+            Assert.False(isPalindrome);
+        }
+    }
+}
---
+++ csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>
+    /// Defines Str Class
+    /// </summary>
+    public class Str
+    {
+        /// <summary>
+        /// Tells if a string is a palindrome
+        /// </summary>
+        /// <param name="s">String to analyse</param>
+        /// <returns>True if s is a palindrome, else False</returns>
+        public static bool IsPalindrome(string s)
+        {
+            string toUpper = s.ToUpper();
+            string onlyUpper = "";
+            for (int i = 0; i < toUpper.Length; i++)
+            {
+                if (toUpper[i] >= 65 && toUpper[i] <= 90)
+                    onlyUpper += toUpper[i];
+            }
+
+            string palindrome = "";
+            for (int i = onlyUpper.Length - 1; i >= 0; i--)
+                palindrome += onlyUpper[i];
+            
+            if (palindrome == onlyUpper)
+                return true;
+            else
+                return false;
+        }
+    }
+}
