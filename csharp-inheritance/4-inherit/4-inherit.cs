f2db7f429bc5ea83cc95d625d185d2935ed9df1d
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-28 10:31:44 +0200

    task 4

---
+++ csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Defines the Animal Class
+/// </summary>
+class Animal
+{
+
+}
+
+/// <summary>
+/// Defines Dog Class
+/// </summary>
+class Dog : Animal
+{
+
+}
