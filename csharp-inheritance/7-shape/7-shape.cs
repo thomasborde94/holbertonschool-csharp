e19f8db3ebe93d7e28bb76c4b907be42f9011b54
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-28 10:58:06 +0200

    task 7

---
+++ csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+/// <summary>
+/// Defines Shape Class
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Gets area if methods in implemented
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    public virtual int Area()
+    {
+        throw new Exception("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// Defines Rectangle Class
+/// </summary>
+class Rectangle : Shape
+{
+    /// <summary>witdh of Rectangle</summary>
+    private int width;
+    /// <summary>height of Rectangle</summary>
+    private int height;
+
+    /// <summary>Gets or sets the width</summary>
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+                width = value;
+        }
+    }
+
+    /// <summary>Gets or sets the height</summary>
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+                height = value;
+        }
+    }
+
+    /// <summary>
+    /// Defines area of Rectangle
+    /// </summary>
+    /// <returns>Area of Rectangle</returns>
+    public new int Area()
+    {
+        return width * height;
+    }
+
+    /// <summary>
+    /// Override ToString method
+    /// </summary>
+    /// <returns>new ToString</returns>
+    public override string ToString()
+    {
+        return string.Format("[Rectangle] {0} / {1}", this.width, this.height);
+    }
+}
--- csharp-inheritance/README.md
+++ csharp-inheritance/README.md
@@ -22,3 +22,17 @@ public static bool IsOnlyASubclass(Type derivedType, Type baseType)
         return derivedType.IsSubclassOf(baseType);
     }
 ```
+
+## Override and New keywords
+When working on derived class, you may want to override an existing method of the base class. You may use override or new keywords to do this.
+* `override` Keyword:
+    * When you use the override keyword, you're indicating that you are providing a new implementation for a method that's already defined in the base class.
+    * The base class method must be marked as virtual, abstract, or override.
+    * The method in the derived class must have the same signature as the method in the base class (name, parameters, and return type).
+    * When calling the method on an instance of the derived class, the overridden method in the derived class will be called instead of the base class method.
+* `new` Keyword:
+    * When you use the new keyword, you're indicating that you're providing a new method with the same name as a method in the base class. This doesn't override the base class method; it simply hides it.
+    * The base class method doesn't need to be marked as virtual, abstract, or override.
+    * The method in the derived class doesn't need to have the same signature as the method in the base class.
+    * When calling the method on an instance of the derived class, the method in the derived class will be called, regardless of the runtime type of the object.
+    * It's used to create a new method in the derived class that has the same name as a method in the base class but behaves differently.
