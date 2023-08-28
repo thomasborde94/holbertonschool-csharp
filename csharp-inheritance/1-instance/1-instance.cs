73abff5214ff5dea5bc6cecca7d6a68ac8b62818
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-28 09:54:00 +0200

    task 1

---
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Defines Obj class
+/// </summary>
+class Obj
+{
+    /// <summary>
+    /// Tells if an object is an instance of, or if the object is an instance
+    /// of a class that inherited from Array
+    /// </summary>
+    /// <param name="obj">Object to analyse</param>
+    /// <returns>True if from Array, else False</returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+}
--- csharp-inheritance/README.md
+++ csharp-inheritance/README.md
@@ -6,3 +6,11 @@ public static bool IsOfTypeInt(object obj)
         return obj.GetType() == typeof(int);
     }
 ```
+
+To verify if an object is an instance of a specific Type, or if the  object is an instance of a class that inherited from a specific class, you can use `is`. For example:
+```
+public static bool IsInstanceOfArray(object obj)
+    {
+        return obj is Array;
+    }
+```
