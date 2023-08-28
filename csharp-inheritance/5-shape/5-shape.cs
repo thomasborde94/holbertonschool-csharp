39075ea9c7e1d7598d168aeb3bb2e076b81aec11
thomasborde94 <thomas.borde94@hotmail.fr>
2023-08-28 10:35:56 +0200

    task 5

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Defines Shape Class
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Gets area if methods in implemented
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    public virtual int Area()
+    {
+        throw new Exception("Area() is not implemented");
+    }
+}
