8a3c4e6ccef6f4cfc0bedb95bebe54d61aaa2515
thomasborde94 <thomas.borde94@hotmail.fr>
2023-09-11 10:25:12 +0200

    task 3

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -4,12 +4,14 @@ class VectorMath
 {
     public static double Magnitude(double[] vector)
     {
+        if (vector.Length > 3 || vector.Length < 2)
+            return -1;
         double resultSqr = 0;
         for (int i = 0; i < vector.Length; i++)
         {
             resultSqr += vector[i] * vector[i];
         }
         double result = Math.Sqrt(resultSqr);
-        return result;
+        return Math.Round(result, 2);
     }
 }
