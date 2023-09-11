1a1e7f1bedf0d1faccbba0a314dba323493aeede
thomasborde94 <thomas.borde94@hotmail.fr>
2023-09-11 13:44:15 +0200

    task 16

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>
+/// Defines MatrixMath class
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Multiplies a scalar and a matrix
+    /// </summary>
+    /// <param name="matrix">matrix to multiply</param>
+    /// <param name="scalar">scalar to multiply</param>
+    /// <returns>result of matrix * scalar</returns>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        double[,] error = {{-1}};
+        if (matrix.Length != 4 && matrix.Length != 9)
+            return error;
+
+        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                result[i, j] = matrix[i, j] * scalar;
+        }
+        return result;
+    }
+}
