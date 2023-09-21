2647098fee7059f56e043ed88b264def56451422
thomasborde94 <thomas.borde94@hotmail.fr>
2023-09-21 09:17:30 +0200

    trying to optimize

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -1,6 +1,8 @@
 ﻿﻿using System;
 using System.Drawing;
 using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 /// <summary>
 /// Defines ImageProcessor class
@@ -13,47 +15,69 @@ class ImageProcessor
         /// <param name="filenames">files to inverse</param>
         public static void Inverse(string[] filenames)
         {
-
-            string currentDirName = System.IO.Directory.GetCurrentDirectory();
-
-            foreach (string filename in filenames)
-            { 
-                string new_filename = filename.Split('.')[0] + "_inverse.jpg";
+            Parallel.ForEach(filenames, filename =>
+            {
+                string newFilename = Path.GetFileNameWithoutExtension(filename) + "_inverse.jpg";
                 Bitmap bmp = new Bitmap(filename);
 
-                //get image dimension
                 int width = bmp.Width;
                 int height = bmp.Height;
 
-                //negative
                 for (int y = 0; y < height; y++)
                 {
                     for (int x = 0; x < width; x++)
                     {
-                        //get pixel value
                         Color p = bmp.GetPixel(x, y);
 
-                        //extract ARGB value from p
                         int a = p.A;
                         int r = p.R;
                         int g = p.G;
                         int b = p.B;
 
-                        //finds negative value
                         int rNegativ = 255 - r;
                         int gNegativ = 255 - g;
                         int bNegativ = 255 - b;
 
-                        //set new ARGB value in pixel
                         bmp.SetPixel(x, y, Color.FromArgb(a, rNegativ, gNegativ, bNegativ));
                     }
                 }
 
-                //save negative image
-                if (new_filename.Split("/").Length > 1)
-                    bmp.Save(new_filename.Split("/")[1]);
+                if (newFilename.Split("/").Length > 1)
+                    bmp.Save(newFilename.Split("/")[1]);
                 else
-                    bmp.Save(new_filename);
+                    bmp.Save(newFilename);
+            });
+        }
+
+        /// <summary>
+        /// Turns a colored image into grayscale
+        /// </summary>
+        /// <param name="filenames">files to modify</param>
+        public static void Grayscale(string[] filenames)
+        {
+            foreach (string filename in filenames)
+            {
+                string newFilename = filename.Split('.')[0] + "_grayscale.jpg";
+                Bitmap bmp = new Bitmap(filename);
+
+                int width = bmp.Width;
+                int height = bmp.Height;
+
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        Color pixel = bmp.GetPixel(x, y);
+
+                        int average = (pixel.R + pixel.G + pixel.B) / 3;
+                        Color grayscalePixel = Color.FromArgb(pixel.A, average, average, average);
+                        bmp.SetPixel(x, y, grayscalePixel);
+                    }
+                }
+                if (newFilename.Split("/").Length > 1)
+                    bmp.Save(newFilename.Split("/")[1]);
+                else
+                    bmp.Save(newFilename);
             }
         }
     }
