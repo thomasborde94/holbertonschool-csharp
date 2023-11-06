1ee1def02d50f16f9bf1bc4868b966f69012c12f
thomasborde94 <thomas.borde94@hotmail.fr>
2023-11-06 11:22:58 +0100

    task 2

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        Type type = this.GetType();
+
+        return ($"{name} is a {type}");
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class Door : Base, IInteractive
+{
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+}
