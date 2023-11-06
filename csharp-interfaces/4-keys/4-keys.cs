da6ebffd70db1d563dae82a1f2143fe3488dd4f4
thomasborde94 <thomas.borde94@hotmail.fr>
2023-11-06 11:27:06 +0100

    task 4

---
+++ csharp-interfaces/4-keys/4-keys.cs
@@ -0,0 +1,121 @@
+﻿﻿using System;
+
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        Type type = this.GetType();
+
+        return ($"{name} is a {type}");
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class Door : Base, IInteractive
+{
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked");
+    }
+}
+
+class Decoration : Base, IInteractive, IBreakable
+{
+    public int durability { get; set; }
+    public bool isQuestItem { get ;set; }
+
+    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
+    {
+        if (durability <= 0)
+        {
+            throw new Exception("Durability must be greater than 0");
+        }
+
+        this.name = name;
+        this.durability = durability;
+        this.isQuestItem = isQuestItem;
+    }
+
+    public void Interact()
+    {
+        if (durability <= 0)
+        {
+            Console.WriteLine($"The {name} has been broken.");
+            return;
+        }
+
+        if (isQuestItem == true)
+        {
+            Console.WriteLine($"You look at the {name}. There's a key inside.");
+        }
+        else
+        {
+            Console.WriteLine($"You look at the {name}. Not much to see here.");
+        }
+    }
+
+    public void Break()
+    {
+        durability--;
+
+        if (durability > 0)
+        {
+            Console.WriteLine($"You hit the {name}. It cracks.");
+        }
+        else if (durability == 0)
+        {
+            Console.WriteLine($"You smash the {name}. What a mess.");
+        }
+        else
+        {
+            Console.WriteLine($"The {name} is already broken.");
+        }
+    }
+}
+
+class Key : Base, ICollectable
+{
+    public bool isCollected { get; set; }
+
+    public Key(string name = "Key", bool isCollected = false)
+    {
+        this.name = name;
+        this.isCollected = isCollected;
+    }
+
+    public void Collect()
+    {
+        if (isCollected == false)
+        {
+            isCollected = true;
+            Console.WriteLine($"You pick up the {name}.");
+        }
+        else
+        {
+            Console.WriteLine($"You have already picked up the {name}.");
+        }
+    }
+}
