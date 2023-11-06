b74ccd98a4d8c08b679d3be4b5c1e600bccdcc19
thomasborde94 <thomas.borde94@hotmail.fr>
2023-11-06 11:19:46 +0100

    task 1

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,52 @@
+﻿﻿using System;
+
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        Type type = this.GetType();
+
+        return ($"{name} is a {type}");
+    }
+}
+
+interface IInteractive
+{
+    void Interact();
+}
+
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Interact()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Break()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Collect()
+    {
+        throw new NotImplementedException();
+    }
+}
