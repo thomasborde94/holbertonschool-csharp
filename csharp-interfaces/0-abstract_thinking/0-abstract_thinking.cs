60d2d80b7806497cbaece4274cec8ec39f0577ec
thomasborde94 <thomas.borde94@hotmail.fr>
2023-11-06 11:16:24 +0100

    task 1

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        Type type = this.GetType();
+
+        return ($"{name} is a {type}");
+    }
+}
