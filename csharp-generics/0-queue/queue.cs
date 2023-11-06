e73abadc93544dd20431d5739f7991b116d9f108
thomasborde94 <thomas.borde94@hotmail.fr>
2023-11-06 10:15:07 +0100

    task 0

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>Generic class</summary>
+class Queue<T>
+{
+    /// <summary>Return type of the Queue class</summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
