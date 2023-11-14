b437f74c12c3137f4b33ea9602b2f1450e20b9c0
thomasborde94 <thomas.borde94@hotmail.fr>
2023-11-14 13:14:34 +0100

    task 1

---
+++ csharp-text_based_interface/InventoryLibrary/Inventory.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+public class Inventory : BaseClass
+{
+    private string user_id;
+    private string item_id;
+    private int _quantity = 1;
+}
---
+++ csharp-text_based_interface/InventoryLibrary/Item.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+public class Item : BaseClass
+{
+    private string name;
+    private string description;
+    private float price;
+    public List<string> tags;
+}
